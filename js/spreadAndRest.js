648cdd3e79e13585ca8b12eaad089a4cfa237214
Ajinkya_Ban <ajinkaban@gmail.com>
2024-02-19 11:22:59 +0530

    final code deployed on master branch

---
+++ js/spreadAndRest.js
@@ -0,0 +1,35 @@
+//Rest operator
+// function Addsum(name,...args)
+// {
+//     // let res = a + b
+//     // console.log(res)
+//     // console.log(arguments)
+//     let sum = 0;
+//     for (let i in args)
+//     {
+//         sum = sum+args[i]
+       
+//     }
+//     console.log(name)
+//     console.log(sum)
+   
+// }
+// Addsum("Hi...😎",2,4)
+// Addsum("JavaScript is awesome..😉",4,5,6)
+
+//Spread operator
+
+// let cars = ['tata','nano','swift']
+// let othercars = ['BMW','Benz']
+
+// // function printAllCars()
+// // {
+// //     // console.log(...cars, ...othercars)
+// //     console.log(othercars)
+// // }
+// // printAllCars();
+
+// newcar = [...othercars]
+// newcar.push('Indica')
+// console.log('Other cars =',othercars)
+// console.log('New car =',newcar)
